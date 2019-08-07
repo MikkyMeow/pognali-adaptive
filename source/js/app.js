2d247b145a321f4e98944410353dd4985cc4a330
Artur Akhmadullin <arturakk2015@gmail.com>
2019-08-07 20:00:45 +0300

    changes

--- source/js/app.js
+++ source/js/app.js
@@ -1,16 +1,28 @@
-var navContent = document.querySelector('.nav__content');
-var navPanel = document.querySelector('.nav__panel');
-var navButton = document.querySelector('.nav__button');
-var navLogin = document.querySelector('.nav__login');
-var nav = document.querySelector('.nav');
+var headerContent = document.querySelector('.header__content');
+var headerPanel = document.querySelector('.header__panel');
+var headerButton = document.querySelector('.header__button');
+var headerLogin = document.querySelector('.header__login');
+var header = document.querySelector('.header');
 
-navContent.classList.add('nav__content--closed');
-navPanel.classList.add('nav__panel--closed');
-navLogin.classList.add('nav__login--closed');
+headerContent.classList.add('header__content--closed');
+headerPanel.classList.add('header__panel--closed');
+headerLogin.classList.add('header__login--closed');
 
-navButton.addEventListener('click', function() {
-  navContent.classList.toggle('nav__content--closed');
-  navPanel.classList.toggle('nav__panel--closed');
-  navLogin.classList.toggle('nav__login--closed');
-  nav.classList.toggle('nav--open');
+headerButton.addEventListener('click', function() {
+  headerContent.classList.toggle('header__content--closed');
+  headerPanel.classList.toggle('header__panel--closed');
+  headerLogin.classList.toggle('header__login--closed');
+  header.classList.toggle('header--open');
+});
+
+var addingPopup = document.querySelector('.adding__popup');
+var addingButton = document.querySelector('.adding__button');
+var addingButtonPopup = document.querySelector('.adding__button--popup');
+
+addingButton.addEventListener('click', function() {
+  addingPopup.classList.toggle('adding__popup--show');
+});
+
+addingButtonPopup.addEventListener('click', function() {
+  addingPopup.classList.toggle('adding__popup--show');
 });
